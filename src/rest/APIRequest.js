0e1553ca47378c78e9e2b05bc8c077acde959771
Koyamie <koyamie1@gmail.com>
2021-08-18 17:35:41 +0200

    AbortController

--- src/rest/APIRequest.js
+++ src/rest/APIRequest.js
@@ -2,6 +2,7 @@
 
 const https = require('https');
 const FormData = require('@discordjs/form-data');
+const AbortController = require('abort-controller');
 const fetch = require('node-fetch');
 const { UserAgent } = require('../util/Constants');
 
