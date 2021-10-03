d60b9a391c08e7dda4e9f9687541facd84f262f2
Koyamie <koyamie1@gmail.com>
2021-10-03 17:21:30 +0200

    Update WebSocketManager.js

--- src/client/websocket/WebSocketManager.js
+++ src/client/websocket/WebSocketManager.js
@@ -1,8 +1,8 @@
 'use strict';
 
+const EventEmitter = require('node:events');
 const { Collection } = require('@discordjs/collection');
 const { RPCErrorCodes } = require('discord-api-types/v9');
-const EventEmitter = require('node:events');
 const WebSocketShard = require('./WebSocketShard');
 const PacketHandlers = require('./handlers');
 const { Error } = require('../../errors');
