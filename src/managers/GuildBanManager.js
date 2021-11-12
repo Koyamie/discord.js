a9caf5ff7b3c2ad69fb9b0955d3fedf2e0a99245
Koyamie <koyamie1@gmail.com>
2021-11-12 01:32:22 +0100

    Update GuildBanManager.js

--- src/managers/GuildBanManager.js
+++ src/managers/GuildBanManager.js
@@ -97,7 +97,7 @@ class GuildBanManager extends CachedManager {
     if (user) return this._fetchSingle({ user, cache: true });
     options.user &&= this.client.users.resolveId(options.user);
     if (!options.user) {
-      if ('cache' in options) return this._fetchMany(options.cache);
+      if ('cache' in options) return this._fetchMany(options);
       return Promise.reject(new Error('FETCH_BAN_RESOLVE_ID'));
     }
     return this._fetchSingle(options);
@@ -113,8 +113,8 @@ class GuildBanManager extends CachedManager {
     return this._add(data, cache);
   }
 
-  async _fetchMany(cache) {
-    if (this.fetchTimestamp && this.fetchTimestamp + 9e5 > Date.now()) return this.cache;
+  async _fetchMany({ cache = true, force = false } = {}) {
+    if (!force && this.fetchTimestamp && this.fetchTimestamp + 9e5 > Date.now()) return this.cache;
     const data = await this.client.api.guilds(this.guild.id).bans.get();
     this.fetchTimestamp = Date.now();
     return data.reduce((col, ban) => col.set(ban.user.id, this._add(ban, cache)), new Collection());
