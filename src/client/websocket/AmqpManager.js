09a09dd77654e978b9cf8748817153fefdf10aad
Koyamie <koyamie1@gmail.com>
2021-12-29 22:30:06 +0100

    init guilds on packet

--- src/client/websocket/AmqpManager.js
+++ src/client/websocket/AmqpManager.js
@@ -18,13 +18,30 @@ class AmqpManager {
     this.connector.on('event', this.handlePacket.bind(this));
   }
 
-  handlePacket(packet) {
+  async handlePacket(packet) {
     console.log(packet);
     if (packet && PacketHandlers[packet.t]) {
+      if (packet.d.guild_id) {
+        await this.initGuild(packet.d);
+      }
       PacketHandlers[packet.t](this.client, packet, packet.shard_id ?? null);
     }
 
     return true;
+  }
+
+  async initGuild(data) {
+    const id = data.guild_id ?? data.id;
+    if (!this.client.guilds.cache.has(id)) {
+      let guild = await this.client.raincache.guild.get(id);
+      if (guild) {
+        guild = this.client.guilds._add(guild);
+        const roles = await this.client.raincache.role.filter((r) => r, guild.id);
+        for (const role of roles) guild.roles._add(role);
+        const channels = await this.client.raincache.channel.filter((c) => c.guild_id === guild.id);
+        for (const channel of channels) this.client.channels._add(channel);
+      }
+    }
   }
 }
 
