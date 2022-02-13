dbc03d58859e4ba53d3b25c64b6646a68e94798b
Koyamie <koyamie1@gmail.com>
2022-02-13 16:11:11 +0100

    fix attachments

--- packages/discord.js/src/structures/CommandInteraction.js
+++ packages/discord.js/src/structures/CommandInteraction.js
@@ -3,6 +3,7 @@
 const { Collection } = require('@discordjs/collection');
 const Interaction = require('./Interaction');
 const InteractionWebhook = require('./InteractionWebhook');
+const MessageAttachment = require('./MessageAttachment');
 const InteractionResponses = require('./interfaces/InteractionResponses');
 const { GuildMember } = require('../structures/GuildMember');
 const { ApplicationCommandOptionTypes } = require('../util/Constants');
@@ -77,6 +78,7 @@ class CommandInteraction extends Interaction {
    * @property {Collection<Snowflake, Role|APIRole>} [roles] The resolved roles
    * @property {Collection<Snowflake, Channel|APIChannel>} [channels] The resolved channels
    * @property {Collection<Snowflake, Message|APIMessage>} [messages] The resolved messages
+   * @property {Collection<Snowflake, MessageAttachment>} [attachments] The resolved attachments
    */
 
   /**
@@ -85,7 +87,7 @@ class CommandInteraction extends Interaction {
    * @returns {CommandInteractionResolvedData}
    * @private
    */
-  transformResolved({ members, users, channels, roles, messages }) {
+  transformResolved({ members, users, channels, roles, messages, attachments }) {
     const result = {};
 
     if (members) {
@@ -124,6 +126,14 @@ class CommandInteraction extends Interaction {
       }
     }
 
+    if (attachments) {
+      result.attachments = new Collection();
+      for (const attachment of Object.values(attachments)) {
+        const patched = new MessageAttachment(attachment.url, attachment.filename, attachment);
+        result.attachments.set(attachment.id, patched);
+      }
+    }
+
     return result;
   }
 
@@ -140,6 +150,7 @@ class CommandInteraction extends Interaction {
    * @property {GuildMember|APIGuildMember} [member] The resolved member
    * @property {GuildChannel|ThreadChannel|APIChannel} [channel] The resolved channel
    * @property {Role|APIRole} [role] The resolved role
+   * @property {MessageAttachment} [attachment] The resolved attachment
    */
 
   /**
@@ -174,6 +185,9 @@ class CommandInteraction extends Interaction {
 
       const role = resolved.roles?.[option.value];
       if (role) result.role = this.guild?.roles._add(role) ?? role;
+
+      const attachment = resolved.attachments?.[option.value];
+      if (attachment) result.attachment = new MessageAttachment(attachment.url, attachment.filename, attachment);
     }
 
     return result;
